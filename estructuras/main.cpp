f750ebfdb984da43ea8f9f5076797f6972a16212
MrShadowDani <dgonzsan@myuax.com>
2023-12-12 22:21:48 +0100

    actualizar

--- estructuras/main.cpp
+++ estructuras/main.cpp
@@ -33,3 +33,55 @@ int main() {
     // d. Punteros y estructuras
     Estudiante* punteroEst = &estudiante1;
     imprimirEstudiante(*punteroEst);
+
+    // e. Organización de la programación
+    // Creación de una lista de estudiantes usando un array
+    const int MAX_ESTUDIANTES = 10;
+    Estudiante listaEstudiantes[MAX_ESTUDIANTES];
+    int cantidadEstudiantes = 0;
+
+    // Función para añadir un estudiante a la lista
+    auto agregarEstudiante = [&](const char* nombre, int edad, float promedio) {
+        if (cantidadEstudiantes < MAX_ESTUDIANTES) {
+            strcpy(listaEstudiantes[cantidadEstudiantes].nombre, nombre);
+            listaEstudiantes[cantidadEstudiantes].edad = edad;
+            listaEstudiantes[cantidadEstudiantes].promedio = promedio;
+            cantidadEstudiantes++;
+        } else {
+            std::cout << "La lista de estudiantes está llena." << std::endl;
+        }
+    };
+
+    // Función para ver los estudiantes en la lista
+    auto verEstudiantes = [&]() {
+        std::cout << "Lista de Estudiantes:" << std::endl;
+        for (int i = 0; i < cantidadEstudiantes; ++i) {
+            imprimirEstudiante(listaEstudiantes[i]);
+        }
+    };
+
+    // Función para eliminar un estudiante de la lista
+    auto eliminarEstudiante = [&](int indice) {
+        if (indice >= 0 && indice < cantidadEstudiantes) {
+            for (int i = indice; i < cantidadEstudiantes - 1; ++i) {
+                listaEstudiantes[i] = listaEstudiantes[i + 1];
+            }
+            cantidadEstudiantes--;
+        } else {
+            std::cout << "Índice inválido." << std::endl;
+        }
+    };
+
+    // Uso de las funciones para manejar la lista de estudiantes
+    agregarEstudiante("Pedro", 21, 8.7);
+    agregarEstudiante("María", 19, 9.1);
+    verEstudiantes();
+
+    eliminarEstudiante(1);
+    verEstudiantes();
+
+    // Liberar memoria reservada con malloc
+    free(estudiante2);
+
+    return 0;
+}
